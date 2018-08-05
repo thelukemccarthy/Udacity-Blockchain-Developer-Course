4e0ed9b01508a842bd498966f290e91c0f8940fc
Luke McCarthy <lmccarth@thoughtworks.com>
2018-08-05 17:27:13 +1000

    Initial commit of project 2 after install of packages and creation of .gitignore file.

---
+++ project 2/simpleChain.spec.js
@@ -0,0 +1,8 @@
+var expect    = require('chai').expect;
+var converter = require('./simpleChain');
+
+describe('simple chain', function() {
+  it('passes', function() {
+    expect(1).to.equal(1);
+  });
+});
